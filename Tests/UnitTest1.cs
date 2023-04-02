55a223c374033e27baed7a40191486bae16a2c27
hrstmr <37159938+hrstmr@users.noreply.github.com>
2023-04-02 13:03:32 +1000

    creates a db snapshot

--- Tests/UnitTest1.cs
+++ Tests/UnitTest1.cs
@@ -12,16 +12,46 @@ public class UnitTest1
     }
 
     [Fact]
-    public void TestDotNetVersion()
+    public async void TestDotNetVersion()
     {
-        Process process = new Process();
-        process.StartInfo.FileName = "dotnet";
-        process.StartInfo.Arguments = "--version";
-        //process.StartInfo.WorkingDirectory = "/path/to/working/directory";
-        process.Start();
-        process.WaitForExit();
-        string output = process.StandardOutput.ReadToEnd();
-        Assert.Contains("5.", output);
+        //dotnet ef migrations add TEMP --project .\Tests\Tests.csproj
+        //dotnet ef dbcontext script -c TestDbContext -p .\Tests\Tests.csproj -o ".\Tests\DbSnapshot.sql"
+        //C:\Work\Projects\dotnet-efcore-check-syned\Tests\bin\Debug\net7.0\Tests\Tests.csproj
+        //C:\Work\Projects\dotnet-efcore-check-syned\Tests\bin\Debug\this\Tests\Tests.csproj
+        //C:\Work\Projects\dotnet-efcore-check-syned\Tests\bin\Debug\this\Tests\Tests.csproj
+        //dotnet ef migrations add TEMP --project ..\..\..\Tests.csproj
+        //dotnet ef migrations add TEMP --no-build --project ..\..\..\Tests.csproj
+        ProcessStartInfo startInfo = new()
+        {
+            FileName = "dotnet",
+            Arguments = "ef migrations add TEMP --json --no-build --project ..\\..\\..\\Tests.csproj",
+            CreateNoWindow = true,
+            RedirectStandardOutput = true,
+            RedirectStandardError = true,
+        };
+        var proc = Process.Start(startInfo);
+        ArgumentNullException.ThrowIfNull(proc);
+        string output = proc.StandardOutput.ReadToEnd();
+        await proc.WaitForExitAsync();
+        Assert.NotNull(output);
+        //TestDbContextModelSnapshot.
+
+        //var builder = new DbContextOptionsBuilder<BloggingContext>();
+
+        //await using var context = new BloggingContext();
+        //{
+
+        //    await context.Database.MigrateAsync();
+        //}
+
+        //Process process = new Process();
+        //process.StartInfo.FileName = "dotnet";
+        //process.StartInfo.Arguments = "--version";
+        ////process.StartInfo.WorkingDirectory = "/path/to/working/directory";
+        //process.Start();
+        //process.WaitForExit();
+        //string output = process.StandardOutput.ReadToEnd();
+        //Assert.Contains("5.", output);
     }
 
 }
